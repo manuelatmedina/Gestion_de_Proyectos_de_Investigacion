c2af60620b4dfcef4f483063e1d24a0d2c384a35
Manuela Tenganàn Medina <manuelamedinat@hotmail.com>
2023-09-05 22:53:11 -0500

    Creacion de Entidades

---
+++ PI.Shared/Entities/Investigator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.Shared.Entities
+{
+    public class Investigator
+    {
+        /*Representa a los científicos y colaboradores que participan en un
+        proyecto de investigación. Debe incluir detalles como nombre, afiliación institucional,
+        especialización, y rol en el proyecto*/
+
+        public int Id { get; set; }
+
+        [Display(Name = "Investigador")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null;
+
+        public string Membership { get; set; } = null;
+        public string Specialization { get; set;} = null;
+        public string Role { get; set; } = null;
+
+
+    }
+}
---
+++ PI.Shared/Entities/Project.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI.Shared.Entities
+{
+    public class Project
+    {
+        /*Esta entidad representa un proyecto de investigación
+        específico. Debe tener atributos como nombre del proyecto, fecha de inicio y
+        finalización, líder del proyecto, descripción y área de investigación.*/
+
+        public int Id { get; set; }
+
+        [Display (Name="Proyecto")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null;
+
+        [Display(Name = "Fecha de Inicio")]
+        [Required(ErrorMessage = "El campo {1} es obligatorio.")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Fecha Finalizacion")]
+        [Required(ErrorMessage = "El campo {2} es obligatorio.")]
+        public DateTime FinishDate { get; set; }
+        public string Owner { get; set; } = null;
+        public string Description { get; set; } = null;
+        public string Department { get; set; } = null;
+
+
+    }
+}
