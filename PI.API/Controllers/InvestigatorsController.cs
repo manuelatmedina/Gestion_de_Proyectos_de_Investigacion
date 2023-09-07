7806e9e4e1aa43aa8bd6ebb2ddbd26d16498175b
Manuela Tenganàn Medina <manuelamedinat@hotmail.com>
2023-09-07 17:01:48 -0500

    Juan David Uribe Gallego Creacion de Controladores y Metodos Get y Set

---
+++ PI.API/Controllers/InvestigatorsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PI.API.Data;
+using PI.Shared.Entities;
+
+namespace PI.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/investigators")]
+    public class InvestigatorsController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public InvestigatorsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Get()
+        {
+            return Ok(await _context.Investigators.ToListAsync());    
+        }
+
+        [HttpPost]
+        public async Task <ActionResult> Post(Investigator investigator)
+        {
+            _context.Add(investigator);
+                await _context.SaveChangesAsync();
+            return Ok(investigator);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            var investigator=await 
+                _context.Investigators.FirstOrDefaultAsync(x => x.Id == Id);
+            if (investigator == null)
+            {
+                return NotFound();
+            }
+            return Ok(investigator);
+        }
+
+
+
+    }
+}
---
+++ PI.API/Controllers/ProjectsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PI.API.Data;
+using PI.Shared.Entities;
+
+namespace PI.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ProjectsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.Projects.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Project project)
+        {
+            _context.Add(project);
+            await _context.SaveChangesAsync();
+            return Ok(project);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            var project = await
+                _context.Projects.FirstOrDefaultAsync(x => x.Id == Id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+    }
+}
